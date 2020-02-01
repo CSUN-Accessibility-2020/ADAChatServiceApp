51f178f0bdce11c77a32695fae2f821cf092201b
Aaron Tiania <aarontiania@gmail.com>
2020-02-01 14:42:00 -0800

    Created "Settings" page
    Settings page accessed from main app screen. Added settings button and back button to navigate to and from the settings page.
    
    Added animations for user experience.
    
    Added new font to test general feel.

---
+++ app/src/main/java/com/mikecodejunky/android/decibel/Settings.java
@@ -0,0 +1,39 @@
+package com.mikecodejunky.android.decibel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.os.Bundle;
+
+
+public class Settings extends AppCompatActivity {
+
+    private ImageButton settingsReturnSwitch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        settingsReturnSwitch = findViewById(R.id.settingsReturnButton);
+
+        settingsReturnSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                moveToApp();
+
+            }
+        });
+    }
+
+    private void moveToApp(){
+
+        Intent intent = new Intent(Settings.this, App.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.push_right_enter,R.anim.push_right_exit);
+    }
+
+}
